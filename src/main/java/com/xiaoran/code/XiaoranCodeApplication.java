a9766badbc44a55b61d6230c544d97cb224b5f8b
aschenxiaoran <as_chenxiaoran@163.com>
2020-04-07 17:31:25 +0800

    提交所有代码

---
+++ src/main/java/com/xiaoran/code/XiaoranCodeApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoran.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoranCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoranCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoran/code/XiaoranCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoran.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoranCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

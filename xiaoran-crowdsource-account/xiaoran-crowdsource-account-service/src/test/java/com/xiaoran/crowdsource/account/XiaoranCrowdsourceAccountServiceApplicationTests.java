b9ec2acabaf69eabac6513d44779646f4963dfe2
aschenxiaoran <as_chenxiaoran@163.com>
2020-04-13 18:14:14 +0800

    第二次提交

---
+++ xiaoran-crowdsource-account/xiaoran-crowdsource-account-service/src/test/java/com/xiaoran/crowdsource/account/XiaoranCrowdsourceAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoran.crowdsource.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoranCrowdsourceAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
